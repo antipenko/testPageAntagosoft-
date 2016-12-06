7a44cd64f54bdb0f9a79ad2fcca634986488492a
antipenko <sweetglory.victory@gmail.com>
2016-12-06 02:01:22 +0200

    init and pre-finish

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,55 @@
 $(document).foundation();
 
+var $newComment = $('#comment'),
+	$commentText = $('#text'),
+	$commentBtn = $('#comment-btn')
+	$commentList =$('#comment-list');
+
+var	$userName = "Скрытый пользователь ";
+	$commentName = "<span class='ts-comment__name'>"+$userName+"</span>";
+	$currentDate = new Date();
+
+var optionsDate = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
+};
+$commentDate = "<time class='ts-comment__date' datetime='"+$currentDate.toLocaleString("ru", optionsDate)+"'>"+ $currentDate.toLocaleString("ru", optionsDate)+ "</time>";
+$comentInfo = "<p class='ts-comment-info'>" + $commentName +" "+ $commentDate +"</p>";
+
+var send = function() {
+     var text = $commentText.val();
+	$commentText.removeClass('error');
+	if (text) {
+		$commentValue = "<p class='ts-comment__text'>"+text+"</p>"
+		$commentList.append($comentInfo).append($commentValue);
+		$commentText.val('');
+	} else $commentText.addClass('error');
+}
+
+$commentBtn.click(function(){
+	send();
+})
+
+// send comment keyboard
+var isCtrl = false;
+$(document).keyup(function (e) {
+ 
+	if(e.which == 17) isCtrl=false;
+}).keydown(function (e) {
+	if(e.which == 17) isCtrl=true;
+	if(e.which == 13 && isCtrl == true) {
+ 		send();
+	}
+ 
+});
+
+
+$('.button-reviews').click(function() {
+	$('.button-reviews').toggleClass("button-reviews_active");
+})
+
+$newComment.click(function(){
+	$commentText.focus();
+})
+
--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,21 @@
  	livereload = require('livereload'),
 	browserSync = require('browser-sync').create(),
 	reload      = browserSync.reload,
- 	rename = require("gulp-rename");
+ 	rename = require("gulp-rename"),
+  spritesmith = require('gulp.spritesmith');
+
+
+gulp.task('sprite', function() {
+    var spriteData = 
+        gulp.src('./app/img/sprite/*.*') // путь, откуда берем картинки для спрайта
+            .pipe(spritesmith({
+                imgName: 'sprite.png',
+                cssName: 'sprite.scss',
+            }));
+
+    spriteData.img.pipe(gulp.dest('./app/img/')); // путь, куда сохраняем картинку
+    spriteData.css.pipe(gulp.dest('./app/scss/')); // путь, куда сохраняем стили
+});
 
 gulp.task('sass', function () {
  return gulp.src('app/scss/**/*.scss')
@@ -30,7 +44,7 @@ gulp.task('serve',function () {
     });
 });
 
-gulp.task('default', ['serve', 'sass'], function() {
+gulp.task('default', ['serve', 'sass', 'sprite'], function() {
 	gulp.watch('app/scss/**/*.scss', ['sass']);
 	gulp.watch("app/*.html").on("change", reload);
     gulp.watch('js/**/*.js').on("change", reload); // Наблюдение за JS файлами в папке js
